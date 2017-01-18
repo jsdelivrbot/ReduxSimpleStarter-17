4b47170e5cc3f2bd4b867cde4192226b1eabb885
trx551 <Keith.Walker@capitalone.com>
2017-01-18 17:43:55 -0500

    Finished through section 2

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import ReactDom from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
-import VideoList from './components/video_list.js';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail'
 const API_KEY = 'AIzaSyAR4sxYudyQX_CyL8DrZbfLTS0dzYgsCrk';
 
 
@@ -12,19 +14,36 @@ class App extends Component {
     constructor(props){
         super(props);
         
-        this.state = { videos: [] };
+        this.state = { 
+            videos: [],
+            selectedVideo: null
+        };
         
-        YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
-            this.setState({ videos });
-            // Same as -> this.setState({ videos: videos});
+        this.videoSearch('surfboards');
+    }
+    
+    videoSearch(term) {
+        YTSearch({key: API_KEY, term: term}, (videos) => {
+            this.setState({ 
+                videos: videos,
+                selectedVideo: videos[0]
+            });
+        // Same as -> this.setState({ videos: videos});
         });
     }
     
     render() {
+        
+        //Creates a debounced function that delays invoking func until after it waits 300 milliseconds since the last time the debounced function was invoked.
+        const videoSearch = _.debounce((term) => {this.videoSearch(term) }, 300)
+        
             return (
         <div>
-            <SearchBar />
-            <VideoList videos={this.state.videos} />
+            <SearchBar onSearchTermChange={videoSearch} />
+            <VideoDetail video={this.state.selectedVideo}/>
+            <VideoList 
+                onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+                videos={this.state.videos} />
         </div>
     );
     }
